05ce33929aa1aceaf4000dcebcc22396b7b3652d
Fernando Meira <thomwork@outlook.com>
2020-08-09 22:35:26 -0300

    feat: finished GiveClasses page

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,13 +1,37 @@
 import React from "react";
-import { View } from "react-native";
-import styles from "../Landing/styles";
+import { useNavigation } from "@react-navigation/native";
 
-import { Container, Title } from "./styles";
+import {
+  Title,
+  Button,
+  OkText,
+  Container,
+  TitleDescription,
+  ImageBackgroundContent,
+} from "./styles";
+import giveClassesBgImage from "../../assets/images/give-classes-background.png";
 
 const GiveClasses = () => {
+  const { goBack } = useNavigation();
+
+  const handleNavigateToLanding = () => {
+    goBack();
+  };
+
   return (
     <Container>
-      <Title>GiveClasses</Title>
+      <ImageBackgroundContent source={giveClassesBgImage}>
+        <Title>Quer ser um Proffy?</Title>
+
+        <TitleDescription>
+          Para começar você precisa se cadastrar como professor na nossa
+          plataforma Web.
+        </TitleDescription>
+      </ImageBackgroundContent>
+
+      <Button>
+        <OkText onPress={handleNavigateToLanding}>Tudo bem</OkText>
+      </Button>
     </Container>
   );
 };
--- mobile/src/pages/GiveClasses/styles.ts
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -1,7 +1,16 @@
 import styled from "styled-components/native";
+import { RectButton } from "react-native-gesture-handler";
 
 export const Container = styled.View`
-  padding: 0 30px;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+
+  background-color: #8257e5;
+`;
+
+export const ImageBackgroundContent = styled.ImageBackground`
+  width: 80%;
 
   flex: 1;
   align-items: center;
@@ -9,6 +18,35 @@ export const Container = styled.View`
 `;
 
 export const Title = styled.Text`
-  color: #333;
-  font-size: 24px;
+  color: #fff;
+  font-size: 32px;
+  line-height: 37px;
+  font-family: "Archivo_700Bold";
+`;
+
+export const TitleDescription = styled.Text`
+  margin-top: 24px;
+
+  color: #d4c2ff;
+  font-size: 16px;
+  line-height: 26px;
+  font-family: "Poppins_400Regular";
+`;
+
+export const Button = styled(RectButton)`
+  width: 90%;
+  height: 58px;
+  margin: 40px 0;
+  border-radius: 8px;
+
+  align-items: center;
+  justify-content: center;
+
+  background-color: #04d361;
+`;
+
+export const OkText = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  font-family: "Archivo_700Bold";
 `;
