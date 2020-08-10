4b708872dd115ae2e9b31cbca041850fea818bd6
Fernando Meira <thomwork@outlook.com>
2020-08-09 21:36:07 -0300

    feat(navigation between pages.): added navigation between pages on project

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View } from "react-native";
+import styles from "../Landing/styles";
+
+import { Container, Title } from "./styles";
+
+const GiveClasses = () => {
+  return (
+    <Container>
+      <Title>GiveClasses</Title>
+    </Container>
+  );
+};
+
+export default GiveClasses;
---
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  padding: 0 30px;
+
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: #333;
+  font-size: 24px;
+`;
